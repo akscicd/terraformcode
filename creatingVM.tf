cfad9481a3bf63d2f2dd9041dd0fa4e253d0fbaa
Amit Kumar Shukla <110124443+akscicd@users.noreply.github.com>
2024-05-22 18:40:56 +0530

    Create creatingVM.tf

---
+++ creatingVM.tf
@@ -0,0 +1,19 @@
+resource "google_compute_instance" "myvm" {
+        name = "terraformcreation"
+        zone = "us-east1-b"
+        machine_type = "e2-medium"
+        boot_disk {
+            initialize_params {
+                image = "ubuntu-os-cloud/ubuntu-2004-lts"
+                labels = {
+                my_label = "value"
+                }
+            }
+        }
+
+         network_interface {
+                network = "default"
+                 access_config {
+                }
+        }
+}
